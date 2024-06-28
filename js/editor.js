eb0a7fc93fbc01651b033c750d0572f1020573be
phlipzamo <phlipzamo14@gmail.com>
2024-06-27 19:22:41 -0700

    text editor added

---
+++ js/editor.js
@@ -0,0 +1,7 @@
+let codeEditor = ace.edit("editorCode",{
+    useWrapMode: true,
+    highlightActiveLine: true,
+    showPrintMargin: false,
+    theme: 'ace/theme/gob',
+    mode: 'ace/mode/javascript'
+});
