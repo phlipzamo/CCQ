1439b0e6184b5732451083e138e13a90c73130be
phlipzamo <phlipzamo14@gmail.com>
2024-07-01 21:06:32 -0700

    autocomplete

--- js/editor.js
+++ js/editor.js
@@ -1,14 +1,26 @@
 
 let codeEditor = ace.edit("editorCode",{
-    useWrapMode: true,
     highlightActiveLine: true,
     showPrintMargin: false,
     theme: 'ace/theme/gob',
-    mode: 'ace/mode/javascript',
-    initialContent: 'hi'
+    mode: 'ace/mode/javascript'
 });
 document.getElementById('editorCode').style.fontSize='40px';
-
+codeEditor.setOptions({
+    enableBasicAutocompletion: [{
+      getCompletions: (editor, session, pos, prefix, callback) => {
+        // note, won't fire if caret is at a word that does not have these letters
+        callback(null, [
+          {value: 'FORWARD', score: 1, meta: 'Moves UFO Forward'},
+          {value: 'BACKWARD', score: 2, meta: 'Moves UFO Forward'},
+          {value: 'ROTATE_RIGHT', score: 2, meta: 'Rotate UFO Right'},
+          {value: 'ROTATE_LEFT', score: 2, meta: 'Rotate UFO Left'},
+        ]);
+      },
+    }],
+    // to make popup appear automatically, without explicit _ctrl+space_
+    enableLiveAutocompletion: true,
+  });
 codeEditor.setValue("");
 codeEditor.session.setUseWrapMode(true);
 var lineSelected = 1;
