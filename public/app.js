980abe5d51af0d7934d8aaa80f2ccab7dcc74008
Dylan Graves <dylanthomasgraves@gmail.com>
2024-07-19 21:15:24 -0700

    saving changes

--- public/app.js
+++ public/app.js
@@ -1,15 +1,36 @@
 // Firebase UI Auth
 
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
-ui.start('#firebaseui-auth-container', {
-    signInOptions: [
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID
-    ],
-    // Other config options...
-});  
+// var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
-
+// var uiConfig = {
+//     callbacks: {
+//       signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+//         // User successfully signed in.
+//         // Return type determines whether we continue the redirect automatically
+//         // or whether we leave that to developer to handle.
+//         return true;
+//       },
+//       uiShown: function() {
+//         // The widget is rendered.
+//         // Hide the loader.
+//         document.getElementById('loader').style.display = 'none';
+//       }
+//     },
+//     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+//     signInFlow: 'popup',
+//     signInSuccessUrl: 'https://www.cosmiccodequest.org/home.html',
+//     signInOptions: [
+//       // Leave the lines as is for the providers you want to offer your users.
+//       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+//       firebase.auth.EmailAuthProvider.PROVIDER_ID,
+//     ],
+//     // Terms of service url.
+//     tosUrl: '<your-tos-url>',
+//     // Privacy policy url.
+//     privacyPolicyUrl: '<your-privacy-policy-url>'
+//   };
+  
+// ui.start('#firebaseui-auth-container', uiConfig);  
 
 
 
@@ -29,9 +50,9 @@ signOutButton.onclick = () => auth.signOut();
 auth.onAuthStateChanged(user => {
     if (user) {
         // signed in
-        window.location.href = "https://cosmiccodequest.org/home.html";
+        // window.location.href = "https://cosmiccodequest.org/home.html";
     } else {
         // not signed in
-        window.location.href = "https://cosmiccodequest.org/index.html";
+        // window.location.href = "https://cosmiccodequest.org/index.html";
     }
 });
